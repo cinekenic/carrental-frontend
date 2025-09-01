1ae67655387cc8e76f8e5cdd441c40f5ff6b4d2f
Piotr-Trzcinski <cinekenic@go2.pl>
2025-09-01 11:48:28 +0200

    added home view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CarListComponent } from './components/car-list.component.ts/car-list.component.ts';
 import { ClientListComponent } from './components/client-list/client-list.component.ts/client-list.component.ts.js';
+import { HomeComponent } from './components/home/home.component.js';
 
 export const routes: Routes = [
-  { path: 'cars', component: CarListComponent },
-  { path: 'clients', component: ClientListComponent },
-  { path: '', redirectTo: 'cars', pathMatch: 'full' },
+  { path: '', component: HomeComponent }, // strona startowa
+  { path: 'cars', component: CarListComponent }, // lista aut
+  { path: 'clients', component: ClientListComponent }, // lista klientów
+  { path: '**', redirectTo: '' }, // fallback
 ];
